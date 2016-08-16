3bbf8332f2ada4e5df5bb0d2d397f9695727e129
Andriy Gapon <avg@icyb.net.ua>
2016-08-16 20:28:43 +0300

    add support for attribute namespaces
    Primarily this means that a support for system attributes is added
    and that attribute names will have namespace prefixes.

--- xattr_freebsd.go
+++ xattr_freebsd.go
@@ -1,17 +1,25 @@
 package xattr
 
 import (
+	"strings"
 	"syscall"
 )
 
+type extattr_namespace int
+
 const (
-	extattr_namespace_user = 1
+	extattr_namespace_user   = extattr_namespace(1)
+	extattr_namespace_system = extattr_namespace(2)
+
+	extattr_namespace_user_prefix   = "user."
+	extattr_namespace_system_prefix = "system."
 )
 
 // Retrieve extended attribute data associated with path.
 func Getxattr(path, name string) ([]byte, error) {
-	// find size.
-	size, err := extattr_get_file(path, extattr_namespace_user, name, nil, 0)
+	namespace, name := parseAttrName(name)
+	// Find size.
+	size, err := extattr_get_file(path, int(namespace), name, nil, 0)
 	if err != nil {
 		return nil, &XAttrError{"extattr_get_file", path, name, err}
 	}
@@ -20,7 +28,7 @@ func Getxattr(path, name string) ([]byte, error) {
 		return buf, nil
 	}
 	// Read into buffer of that size.
-	read, err := extattr_get_file(path, extattr_namespace_user, name, &buf[0], size)
+	read, err := extattr_get_file(path, int(namespace), name, &buf[0], size)
 	if err != nil {
 		return nil, &XAttrError{"extattr_get_file", path, name, err}
 	}
@@ -30,8 +38,28 @@ func Getxattr(path, name string) ([]byte, error) {
 // Retrieves a list of names of extended attributes associated with the
 // given path in the file system.
 func Listxattr(path string) ([]string, error) {
-	// find size.
-	size, err := extattr_list_file(path, extattr_namespace_user, nil, 0)
+	userlist, err := listxattr_impl(path, extattr_namespace_user)
+	if err != nil {
+		return nil, err
+	}
+	systemlist, err := listxattr_impl(path, extattr_namespace_system)
+	// Regular users are not allowed to see system attributes.
+	if err != nil && err.Err != syscall.EPERM {
+		return nil, err
+	}
+	list := make([]string, 0, len(userlist)+len(systemlist))
+	for _, name := range userlist {
+		list = append(list, extattr_namespace_user_prefix+name)
+	}
+	for _, name := range systemlist {
+		list = append(list, extattr_namespace_system_prefix+name)
+	}
+	return list, nil
+}
+
+func listxattr_impl(path string, namespace extattr_namespace) ([]string, *XAttrError) {
+	// Find size.
+	size, err := extattr_list_file(path, int(namespace), nil, 0)
 	if err != nil {
 		return nil, &XAttrError{"extattr_list_file", path, "", err}
 	}
@@ -40,7 +68,7 @@ func Listxattr(path string) ([]string, error) {
 	}
 	buf := make([]byte, size)
 	// Read into buffer of that size.
-	read, err := extattr_list_file(path, extattr_namespace_user, &buf[0], size)
+	read, err := extattr_list_file(path, int(namespace), &buf[0], size)
 	if err != nil {
 		return nil, &XAttrError{"extattr_list_file", path, "", err}
 	}
@@ -49,7 +77,8 @@ func Listxattr(path string) ([]string, error) {
 
 // Associates name and data together as an attribute of path.
 func Setxattr(path, name string, data []byte) error {
-	written, err := extattr_set_file(path, extattr_namespace_user, name, &data[0], len(data))
+	namespace, name := parseAttrName(name)
+	written, err := extattr_set_file(path, int(namespace), name, &data[0], len(data))
 	if err != nil {
 		return &XAttrError{"extattr_set_file", path, name, err}
 	}
@@ -61,10 +90,27 @@ func Setxattr(path, name string, data []byte) error {
 
 // Remove the attribute.
 func Removexattr(path, name string) error {
-	if err := extattr_delete_file(path, extattr_namespace_user, name); err != nil {
+	namespace, name := parseAttrName(name)
+	if err := extattr_delete_file(path, int(namespace), name); err != nil {
 		return &XAttrError{"extattr_delete_file", path, name, err}
 	}
 	return nil
+}
+
+// Check if the attribute name has a predefined prefix, trim the prefix
+// and return a namespace identifier corresponding to the prefix.
+// Treat a name with no recognized prefix as an attribute name
+// in the user namespace.
+func parseAttrName(name string) (extattr_namespace, string) {
+	if strings.HasPrefix(name, extattr_namespace_user_prefix) {
+		name := strings.TrimPrefix(name, extattr_namespace_user_prefix)
+		return extattr_namespace_user, name
+	}
+	if strings.HasPrefix(name, extattr_namespace_system_prefix) {
+		name := strings.TrimPrefix(name, extattr_namespace_system_prefix)
+		return extattr_namespace_system, name
+	}
+	return extattr_namespace_user, name
 }
 
 // Convert a sequnce of attribute name entries to a []string.
